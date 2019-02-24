f6aa1bd6f8837333ace5ef0962f886d52c028410
ltt <641518695@qq.com>
2019-02-24 11:49:48 +0800

    2019-2-24 11:49  test2

---
+++ GitTest/src/main/java/gittest2/GitTest2.java
@@ -0,0 +1,13 @@
+package gittest2;
+
+/**
+ * @Author: ltt
+ * @Description:
+ * @Date: Created in 11:42 2019/2/24
+ * @Modified By:
+ */
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("gittest2");
+    }
+}
